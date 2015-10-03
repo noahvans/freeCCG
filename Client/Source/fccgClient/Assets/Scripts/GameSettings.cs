aa8de8912b710753283484697dd3bd205fe0dc27
noahvans <noahvans@gmail.com>
2015-10-03 16:43:44 +0800

    Some very primary works

---
+++ Client/Source/fccgClient/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Noahv.Game.Fccg
+{
+    //A class to store game settings
+    [Serializable]
+    public class GameSettings
+    {
+        private static GameSettings m_Current = null;
+        public static GameSettings Current { get { return m_Current; } }
+
+        //Load saved game setting file
+        static GameSettings()
+        {
+            var configFilePath = Application.persistentDataPath + Path.DirectorySeparatorChar + "GameSettings.cnf";
+            try
+            {
+                BinaryFormatter binFormat = new BinaryFormatter();
+                if (File.Exists(configFilePath))
+                    using (var fsGameSettings = File.OpenRead(configFilePath))
+                    {
+                        m_Current = (GameSettings)binFormat.Deserialize(fsGameSettings);
+                        fsGameSettings.Close();
+                    }
+                else m_Current = new GameSettings();
+            }
+            catch
+            {
+                m_Current = new GameSettings();
+            }
+        }
+
+        //Save current game settings to file
+        public bool Save()
+        {
+            var configFilePath = Application.persistentDataPath + Path.DirectorySeparatorChar + "GameSettings.cnf";
+            try
+            {
+                BinaryFormatter binFormat = new BinaryFormatter();
+                using (var fsGameSettings = File.OpenWrite(configFilePath))
+                {
+                    binFormat.Serialize(fsGameSettings, this);
+                    fsGameSettings.Close();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Language
+        private UILanguage m_Language = UILanguage.en_US;
+        public UILanguage Language
+        {
+            get { return m_Language; }
+            set { m_Language = value; }
+        }
+    }
+}

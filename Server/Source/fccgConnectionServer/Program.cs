79454b8e9dae07cd0fd847ee52c843a73170cfc0
noahvans <noahvans@gmail.com>
2015-10-13 03:11:00 +0800

    include VS2015 solution/project files

---
+++ Server/Source/fccgConnectionServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Noahv.Game.Fccg.fccgConnectionServer
+{
+    public class Program
+    {
+        private System.Net.IPAddress serverIP = System.Net.IPAddress.Parse("127.0.0.1");
+        private int serverPort = 62501;
+
+        public void Main(string[] args)
+        {
+            //if (args == null || args.Length == 0)
+            //    Console.WriteLine("arguments: port");
+        }
+    }
+}
---
+++ Server/Source/fccgConnectionServer/TcpServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Noahv.Game.Fccg.fccgConnectionServer
+{
+    /// <summary>
+    /// This is a Tcp version of the connection server
+    /// </summary>
+    public class TcpServer
+    {
+        public void Run(int port)
+        {
+            Thread serverSocketThraed = new Thread(() =>
+            {
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind(new IPEndPoint(IPAddress.Any, port));
+                server.Listen(10);
+                //server.BeginAccept(new AsyncCallback(Accept), server);                
+            });
+
+            serverSocketThraed.Start();
+            Console.WriteLine("Server is ready");
+            //Broadcast();
+        }
+    }
+}
